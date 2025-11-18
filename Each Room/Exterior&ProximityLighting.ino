16492050615746e5dd185e2e642f7608253b1cd6
Austria-Hungary15 <t0612560280@gmail.com>
2025-11-18 20:42:05 +0700

    Update Exterior&ProximityLighting.ino

--- Each Room/Exterior&ProximityLighting.ino
+++ Each Room/Exterior&ProximityLighting.ino
@@ -1,8 +1,9 @@
 #include <IROVER.h>
 #include <Adafruit_NeoPixel.h>
 
-const int Ultra = 32;
 const int LDR = 33;
+const int Ultra = 32;
+
 const int LED1 = 5;
 const int LED2 = 23;
 
@@ -12,7 +13,7 @@ void setup() {
 }
 
 int daynightcheck(){
-  if(analogRead(LDR) <= 1000){
+  if(analogRead(LDR) >= 1000){
     return 0;
   }else{
     return 1;
@@ -21,20 +22,18 @@ int daynightcheck(){
 
 void ExteriorLight(int a){
   if(a == 0){
-    digitalWrite(LED1,HIGH);
     digitalWrite(LED2,HIGH);
   }else{
-    digitalWrite(LED1,LOW);
     digitalWrite(LED2,LOW);
   }
 }
 
 void ProximityLight(int a){
   if(a == 0){
-    if(analogRead(Ultra) <= 1000){
-      digitalWrite(LED2,HIGH);
+    if(analogRead(Ultra) <= 500){
+      digitalWrite(LED1,HIGH);
     }else{
-      digitalWrite(LED2,LOW);
+      digitalWrite(LED1,LOW);
     }
   }
 }
