d6b3abcea153e3ddbd07380e53a3c1a798dc03f1
Austria-Hungary15 <t0612560280@gmail.com>
2025-12-01 09:27:02 +0700

    Add GateButton and IR sensor functionality

--- All system.ino
+++ All system.ino
@@ -1,6 +1,7 @@
 #include <IROVER.h>
 #include <Adafruit_NeoPixel.h>
 
+
 const int PixelRing = 12;
 const int PixelPin = 19;
 
@@ -11,16 +12,21 @@ const int SoundSensor = 35;
 const int LDR = 33;
 const int Ultra = 32;
 
+const int GateButton = i0;
+const int IR = i1;
+
 const int LED1 = 5;
 const int LED2 = 23;
 
 int BedroomN = 1;
+int Gate = 0;
 
 void setup() {
-  
+  init(0x48);
   pixels.begin();
   pixels.setBrightness(50);
   pinMode(BedroomButton, INPUT_PULLUP);
+  pinMode(GateButton, INPUT_PULLUP);
   pinMode(LED1,OUTPUT);
   pinMode(LED2,OUTPUT);
 }
@@ -80,8 +86,51 @@ void ProximityLight(int a){
   }
 }
 
+/*void Garage(){
+  if(digitalRead() == 0){
+    if(!(GarageState)){
+      GarageState = true;
+      digitalWrite( ,HIGH);
+      for(int i = 1;i<= ;i++){
+        tone(Buzzer,);
+        motor(1,30);
+        delay(100);
+        noTone(Buzzer);
+        delay(100);
+      }
+      motor(1,0);
+    }else{
+      GarageState = false;
+      digitalWrite( ,LOW);
+      for(int i = 1;i<= ;i++){
+        tone(Buzzer,);
+        motor(1,-30);
+        delay(100);
+        noTone(Buzzer);
+        delay(100);
+      }
+      motor(1,0);
+    }
+  }
+}*/
+
+void GateControl(){
+  if(digitalRead(GateButton) == 0 || analogRead(IR) >= 1500){
+    if(Gate == 0){
+      servo(10,0);
+      Gate = 1;
+    }else{
+      servo(10,45);
+      Gate = 0;
+    }
+    delay(200);
+  }
+}
+
 void loop() {
   ExteriorLight(daynightcheck());
   ProximityLight(daynightcheck());
   Bedroom(BedroomControl());
+  //Garage()
+  GateControl();
 }
